f93e59d34a2b401c3bf50c9cc8fee67bfb4f015a
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-09 19:56:20 +0200

    add payments component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import Payments from './Payments';
+
 class Header extends React.Component {
   renderContent() {
     switch (this.props.auth) {
