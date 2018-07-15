7582a2a08e52184b44755ba27d072075ebeb4a0b
Adam Marczyk <adam.marczyk90@gmail.com>
2018-07-15 21:06:01 +0200

    unique events

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -27,6 +27,11 @@ module.exports = app => {
         };
       }
     });
+
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
